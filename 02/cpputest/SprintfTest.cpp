7559b09354c0bdbdf6e270c55cda9558388a91e1
Bukuli Norbert <bukuli.norbert@protecta.hu>
2015-02-16 09:32:12 +0100

    02: CppUTest: Insert format string operation

--- 02/cpputest/SprintfTest.cpp
+++ 02/cpputest/SprintfTest.cpp
@@ -7,3 +7,11 @@ TEST(sprintf, NoFormatOperations)
 	LONGS_EQUAL(3, sprintf(output, "hey"));
 	STRCMP_EQUAL("hey", output);
 }
+
+TEST(sprintf, InsertStringFormatOperation)
+{
+	char output[20] = "";
+
+	LONGS_EQUAL(12, sprintf(output, "Hello %s\n", "World"));
+	STRCMP_EQUAL("Hello World\n", output);
+}
