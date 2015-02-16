d20d28f242cd6b7b061a5d43d2d8698217dba8f2
Bukuli Norbert <bukuli.norbert@protecta.hu>
2015-02-16 09:33:10 +0100

    02: CppUTest: Tulfutas figyeles

--- 02/cpputest/SprintfTest.cpp
+++ 02/cpputest/SprintfTest.cpp
@@ -3,15 +3,19 @@
 TEST(sprintf, NoFormatOperations)
 {
 	char output[5] = "";
+	memset(output, 0xaa, sizeof output);
 
 	LONGS_EQUAL(3, sprintf(output, "hey"));
 	STRCMP_EQUAL("hey", output);
+	BYTES_EQUAL(0xaa, output[4]);
 }
 
 TEST(sprintf, InsertStringFormatOperation)
 {
 	char output[20] = "";
+	memset(output, 0xaa, sizeof output);
 
 	LONGS_EQUAL(12, sprintf(output, "Hello %s\n", "World"));
 	STRCMP_EQUAL("Hello World\n", output);
+	BYTES_EQUAL(0xaa, output[13]);
 }
