11f7b7af1475c4d6697a3be171c75be8ad642f6c
Bukuli Norbert <bukuli.norbert@protecta.hu>
2015-02-16 09:14:46 +0100

    02: Minimalizalt kod
    A given() es expect() seged fugvenyek hasznalatba lettek veve.

--- 02/unity/SprintfTest.c
+++ 02/unity/SprintfTest.c
@@ -29,20 +29,12 @@ static void given(int charsWritten)
 /* ---------- */
 TEST(sprintf, NoFormatOperations)
 {
-	char output[5] = "";
-	memset(output, 0xaa, sizeof output);
-
-	TEST_ASSERT_EQUAL(3, sprintf(output, "hey"));
-	TEST_ASSERT_EQUAL_STRING("hey", output);
-	TEST_ASSERT_BYTES_EQUAL(0xaa, output[4]);
+	expect("hey");
+	given(sprintf(output, "hey"));
 }
 
 TEST(sprintf, InsertString)
 {
-	char output[20] = "";
-	memset(output, 0xaa, sizeof output);
-
-	TEST_ASSERT_EQUAL(12, sprintf(output, "Hello %s\n", "World"));
-	TEST_ASSERT_EQUAL_STRING("Hello World\n", output);
-	TEST_ASSERT_BYTES_EQUAL(0xaa, output[13]);
+	expect("Hello World\n");
+	given(sprintf(output, "Hello %s\n", "World"));
 }
