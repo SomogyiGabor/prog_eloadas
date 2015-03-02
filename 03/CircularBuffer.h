82fd4b25def4d0be78fd8be8e4250b3700c27429
Bukuli Norbert <bukuli.norbert@protecta.hu>
2015-03-02 09:51:40 +0100

    Operations for circular buffer

--- 03/CircularBuffer.h
+++ 03/CircularBuffer.h
@@ -4,4 +4,7 @@
 struct CircularBufferStruct;
 typedef struct CircularBufferStruct CircularBuffer;
 
+int CircularBuffer_Push(CircularBuffer self, int valueToPush);
+int CircularBuffer_Pop(CircularBuffer self, int* poppedValue);
+
 #endif
