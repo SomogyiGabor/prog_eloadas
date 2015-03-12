232f177eed2dc687c501c42441971db189bb03c2
Bukuli Norbert <bukuli.norbert@protecta.hu>
2015-03-12 16:23:01 +0100

    04_1: Failing test for turning on multiple LEDs

---
+++ 04/LedDriver.h
@@ -0,0 +1,11 @@
+#ifndef LEDDRIVER_H
+#define LEDDRIVER_H 1
+
+#include <stdint.h>
+
+void LedDriver_Create(uint16_t* address);
+
+void LedDriver_TurnOn(int ledNumber);
+void LedDriver_TurnOff(int ledNumber);
+
+#endif
---
+++ 04/Test/LedDriverTest.cpp
@@ -0,0 +1,53 @@
+#include <CppUTest/CommandLineTestRunner.h>
+#include <CppUTest/TestHarness.h>
+
+extern "C"
+{
+#include "LedDriver.h"
+}
+
+TEST_GROUP(LedDriver)
+{
+	uint16_t virtualLeds;
+
+	TEST_SETUP()
+	{
+		LedDriver_Create(&virtualLeds);
+	}
+
+	TEST_TEARDOWN()
+	{}
+};
+
+TEST(LedDriver, LedsOffAfterCreate)
+{
+	uint16_t virtualLeds = 0xffff;
+	LedDriver_Create(&virtualLeds);
+	CHECK_EQUAL(0x0000, virtualLeds);
+}
+
+TEST(LedDriver, TurnOnLedOne)
+{
+	LedDriver_TurnOn(1);
+	CHECK_EQUAL(0x0001, virtualLeds);
+}
+
+TEST(LedDriver, TurnOffLedOne)
+{
+	LedDriver_TurnOn(1);
+	LedDriver_TurnOff(1);
+	CHECK_EQUAL(0x0000, virtualLeds);
+}
+
+TEST(LedDriver, TurnOnMultipleTests)
+{
+	LedDriver_TurnOn(9);
+	LedDriver_TurnOn(8);
+
+	CHECK_EQUAL(0x180, virtualLeds);
+}
+
+int main(const int argc, const char* argv[])
+{
+	return CommandLineTestRunner::RunAllTests(argc, argv);
+}
