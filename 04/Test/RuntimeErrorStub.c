252573d84151246226ade013738b49d11cede0ea
Bukuli Norbert <bukuli.norbert@protecta.hu>
2015-03-12 17:58:19 +0100

    04_17: Stub implementation of RuntimeError()

---
+++ 04/Test/RuntimeErrorStub.c
@@ -0,0 +1,34 @@
+#include "RuntimeErrorStub.h"
+
+static const char* message = "No Error";
+static int parameter = -1;
+const char* file = NULL;
+static int line = -1;
+
+void RuntimeErrorStub_Reset()
+{
+	message = "No Error";
+	parameter = -1;
+}
+
+const char* RuntimeErrorStub_GetLastError()
+{
+	return message;
+}
+
+int RuntimeErrorStub_GetLastParameter()
+{
+	return parameter;
+}
+
+
+void RuntimeError(const char* aMessage,
+		  int aParameter,
+		  const char* aFile,
+		  int aLine)
+{
+	message = aMessage;
+	parameter = aParameter;
+	file = aFile;
+	line = aLine;
+}
