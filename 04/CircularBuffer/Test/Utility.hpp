b9ef55fb75dfd6967519dbd2e89df49278c4378d
Bukuli Norbert <bukuli.norbert@protecta.hu>
2015-03-24 14:55:25 +0100

    ch04: CircularBuffer: Teszt lista helyett IGNORE_TEST
    Az utolso elvarasrol (hasznalt meret) lemondtunk

---
+++ 04/CircularBuffer/Test/Utility.hpp
@@ -0,0 +1,32 @@
+#ifndef UTILITY_HPP
+#define UTILITY_HPP 1
+
+extern "C"
+{
+#include "CircularBuffer.h"
+}
+
+#include <CppUTest/TestHarness.h>
+
+TEST_GROUP(Utility)
+{};
+
+TEST(Utility, NewlyCreatedIsEmpty)
+{
+	CircularBuffer_Create(10);
+	CHECK_TRUE(CircularBuffer_Empty());
+}
+
+TEST(Utility, NewlyCreatedIsNotFull)
+{
+	CircularBuffer_Create(10);
+	CHECK_FALSE(CircularBuffer_Full());
+}
+
+IGNORE_TEST(Utility, PushOneElementMakesBufferNotEmpty)
+{}
+
+IGNORE_TEST(Utility, FullBufferIsFull)
+{}
+
+#endif
